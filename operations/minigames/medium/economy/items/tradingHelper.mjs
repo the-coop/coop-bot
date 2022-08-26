41113cdedbde545fcdae96f5411abd1ef8f660a2
L M Fielding <contact@lmfielding.com>
2022-08-27 00:34:57 +0100

    Fix trading error

--- operations/minigames/medium/economy/items/tradingHelper.mjs
+++ operations/minigames/medium/economy/items/tradingHelper.mjs
@@ -29,7 +29,7 @@ export default class TradingHelper {
         // return editResult;
 
         // Post latest/most recent 5-10 trades in talk.
-        const lastTrades = await this.all();
+        const lastTrades = await Trading.all();
         if (lastTrades.length > 0)
             CHANNELS._send('TALK', '**Latest active trades**:\n' + this.manyTradeItemsStr(lastTrades), 0, 30000);
     }
