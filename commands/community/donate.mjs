ec9a71076b403afe605fc93e1a52b81be7013268
L M Fielding <contact@lmfielding.com>
2022-10-02 02:53:34 +0100

    Add donate command

---
+++ commands/community/donate.mjs
@@ -0,0 +1,14 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+export const name = 'donate';
+
+export const description = 'Prompt shilling for 2 servers (game + bot/api), domain and database!';
+    
+export const data = new SlashCommandBuilder()
+	.setName(name)
+	.setDescription(description);
+
+export const execute = async (interaction) => {
+	const shillingText = `I'm sorry, I can't shill right now, working on copywriting for the begging`;
+	return await interaction.reply(shillingText);
+};
