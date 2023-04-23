b62a318a35f7a3de36baf58453c543402eef8611
L M Fielding <contact@lmfielding.com>
2023-04-23 15:23:39 -0300

    Clean up deps

--- setup/shallow.mjs
+++ setup/shallow.mjs
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import { GatewayIntentBits, Client, ActionRowBuilder, ButtonBuilder, ButtonStyle, SelectMenuBuilder, REST, Routes, WebhookClient } from 'discord.js';
+import { GatewayIntentBits, Client, ActionRowBuilder, ButtonBuilder, ButtonStyle, SelectMenuBuilder, REST, Routes, WebhookClient, ChannelType, PermissionFlagsBits } from 'discord.js';
 import _ from 'lodash';
 import Database from 'coop-shared/setup/database.mjs';
 import COOP, { CHANNELS, CHICKEN, ITEMS, MESSAGES, REACTIONS, ROLES, SERVER, STATE, TIME, USERS } from '../coop.mjs';
@@ -69,73 +69,42 @@ const shallowBot = async () => {
     COOP.STATE.CLIENT.on('ready', async () => {
         console.log('Shallow bot is ready');
 
+        // TODO: Get list of channels/categories.
+
+        // const channels = CHANNELS.config;
+        // console.log(channels);
+
+        const coop = SERVER._coop();
+        // const categoryChannels = coop.channels.cache.filter(channel => channel.type === ChannelType.GuildCategory);
+        // categoryChannels.map(c => console.log(c.name, c.id));
+
+
+        // coop.channels.create({
+        //     name: 'tasks|🔨',
+        //     type: ChannelType.GuildForum,
+        //     parent: '1097911978560856165'
+        // });
+
+        // coop.channels.cache.get('1097915532814057502').delete();
+        
+
+        // Agency | 🏭 1097911978560856165
+
+        // TODO: Check if agency role still exists.
+
+        // const agency = CHANNELS._get('1097911978560856165');
+        // console.log(agency.name, agency.id);
+
+
+        // TODO: Restrict permissions to AGENCY role
+
+
         // const txsPrevDay = await CHICKEN.getTransactionsPreviousDay();
         // const summarisedTxs = ActivityHelper.summariseTransactions(txsPrevDay);
 
-        const msg = await CHANNELS._send('TALK', 'TEST');
-        msg.edit({ components: [
-            new ActionRowBuilder().addComponents([
-                new ButtonBuilder()
-                    .setEmoji('<:we_are:725005855614959709>')
-                    .setLabel('Testing')
-                    .setURL("https://www.thecoop.group/guide/minigames")
-                    .setStyle(ButtonStyle.Link)
-            ])
-        ] });
-
-        // BlogHelper.postToReddit('TESTIgewawegwegawgwNG', 'COgwegawegwagwegewggwNTENT');
-        
-        // console.log(await BlogHelper.redditAccessToken());
-        // await BlogHelper.test();
-        // static add(member, roleCode) {
-        // const guild = SERVER._coop();
-        // const role = this.getRoleByID(guild, ROLES_CONFIG[roleCode].id);
-        // return member.roles.add(role);
-
-        // const member = USERS._get('308367247447949333');
-        // console.log(member);
-
-        // ROLES.add(member, 'MEMBER')
-        // 308367247447949333
-
-        // const announceRole = ROLES._getByCode('SUBSCRIBER');
-        // console.log(announceRole);
-
-        // const electionText = `our latest ${CHANNELS.textRef('ELECTION')} is running, all members are welcome to stand or vote for their preferred commander and leaders. \n` +
-        //     `For further information on our elections refer to our forth amendment in ${CHANNELS.textRef('ABOUT')}\n\n` +
-
-        //     `To enter election yourself use the /stand slash command with a short message of why people should vote for you.\n` +
-        //     `To nominate another person for leadership use the /nominate slash command with a similar message.\n\n`;
-
-        // const msg = await CHANNELS._send(
-        //     'TALK', 
-        //     `${ROLES._textRef('SUBSCRIBER')}, ${electionText}`,
-        //     { allowedMentions: { roles: [announceRole.id] }}
-        // );
-
-        // SacrificeHelper.random();
-
-        // const offer = (await SacrificeHelper.loadOffers())[0];
-        // const message = await MESSAGES.getByLink(offer.message_link);
-
-        // console.log(message.embeds[0].data.description);
-
-        // const desc = message.embeds[0].data.description;
-        // const discordID = /<@(\d*)>/.exec(desc)[1];
-
-
-        // Items.add('786671654721683517', 'LEADERS_SWORD', 1, 'Left accidentally, still a valid leader');
-
         // TODO: Check the crate is included in temp messages and thus preloaded?
 
-        // ElectionHelper.ensureItemSeriousness();
 
-        // RedditHelper.prompt();
-
-        // RedditHelper.codeToToken('zCG8I_n5dIHPAkxM1aMQHLOd1KRupw#')
-
-        // tqTfiVcnOKXqzQVhXWEw5j6U8glu0A#_
-        
     });
 };
 
