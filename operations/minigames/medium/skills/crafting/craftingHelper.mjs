1708512821d229574e21466043f977b905c7c735
JP <122294168+XOWithSauce@users.noreply.github.com>
2024-10-07 17:14:11 +0300

    Make Key craftable with 1 iron bar

--- operations/minigames/medium/skills/crafting/craftingHelper.mjs
+++ operations/minigames/medium/skills/crafting/craftingHelper.mjs
@@ -47,6 +47,13 @@ export default class CraftingHelper {
                 STEEL_BAR: 3,
                 IRON_BAR: 5
             }
+        },
+        KEY: {
+            levelReq:1,
+            xpReward:1,
+            ingredients: {
+                IRON_BAR:1
+            }
         }
     }
 
